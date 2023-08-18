6259c9666f0a18ada62aaa1b709ebe1c8520c4d4
unknown <daniellsilva@outlook.com>
2023-08-18 20:51:38 -0300

    chore: fix SQL script - not creating the database

--- app/database/DBUser_User.sql
+++ app/database/DBUser_User.sql
@@ -16,6 +16,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Create database `DBUser`
+--
+CREATE DATABASE DBUser;
+USE DBUser;
+
+--
 -- Table structure for table `User`
 --
 
